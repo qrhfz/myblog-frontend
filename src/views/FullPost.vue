bd364381a0c8e9778cce5d50082f9b612e8c41e2
Qori El-Hafizh <qori.elhafizh@gmail.com>
2021-07-22 12:29:55 +0700

    tambah tanggal di postitem

--- src/views/FullPost.vue
+++ src/views/FullPost.vue
@@ -1,18 +1,34 @@
 <template>
-  <div>Full Post</div>
+  <div id="full-post">
+    <div v-if="post">
+      {{post.body}}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from "vue";
+import Post from "@/types/Post";
+import axios from "axios";
+import { useRoute } from 'vue-router'
 
 export default defineComponent({
-    // setup() {
-        
-    // },
-})
+  
+  setup() {
+    const route = useRoute()
+    const post = ref<Post>();
+    onMounted(() => {
+      const id = route.params.id
+      axios
+        .get("http://localhost:3000/post/" + id)
+        .then((res) => (post.value = res.data))
+        .catch((e) => console.log(e));
+    });
+    return { post };
+  },
+});
 </script>
 
 
 <style>
-
 </style>
